b10512153f627c1df18e7d8c735a9043d7f7049e
Vaibhaviii14 <vaibhavi.s147@gmail.com>
2025-12-22 18:02:24 +0530

    Changed Our faculty section

--- src/components/home/MessageSection.jsx
+++ src/components/home/MessageSection.jsx
@@ -8,11 +8,11 @@ import tpcHeadImage from "@/assets/tpc-head.jpg";
 
 const messages = [
   {
-    image: directorImage,//change the image when you get it
-    name: "Dr. Samir Bajpai",
-    role: "Head, Career Development Center",
+    image: directorImage,
+    name: "Dr. N V Ramana Rao",
+    role: "Director, NIT Raipur",
     message:
-      "Industry and Institute collaborations and linkages are a must for sustainable growth of technology, economy, and human civilisation. NIT Raipur is sincerely working for symbiotic relationship with industries since its inception in 1956. Campus placement process is an important aspect of this relationship and further strengthening this relationship by undertaking a visit to our Institute for the campus placement of our students.",
+      "Industry and Institute collaborations and linkages are a must for sustainable growth of technology, economy, and human civilisation. NIT Raipur is sincerely working for symbiotic relationship with industries since its inception in 1956. Campus placement process is an important aspect of this relationship and further strengthening this relationship by undertaking a visit to our Institute for the campus placement of our students. NIT Raipur, established as one of the earliest engineering colleges of independent India, has continuously strived to achieve excellence in technology education and research; in the process, it has carved a niche for itself on the national level. On account of its academic atmosphere, well qualified and committed faculty members, regular extra-curricular activities and regular and examination procedure, the institute ensures the holistic development of students. The distinguished alumni of the institute have contributed significantly to various fields such as industry, academia, and administration and bear a testimony to the caliber of the institution. I am confident that a placement visit to our campus will be mutually beneficial and students of the institute will prove to be an asset to your organization. I heartily welcome you to NIT Raipur and hope that this interaction leads to a lasting partnership between our institute and your organisation.",
   },
   // {
   //   image: tpcHeadImage,
@@ -40,7 +40,7 @@ export const MessageSection = () => {
         </motion.div>
 
         <div className="flex justify-center">
-          <div className="max-w-[800px] w-full">
+          <div className="max-w-[1000px] w-full">
           {messages.map((item, index) => (
             <motion.div
   key={index}
